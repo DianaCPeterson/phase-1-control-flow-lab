6414885bdf3f571de2c04cc4f227dad7bc67e751
Diana C Peterson <dpeter10@gmail.com>
2022-10-30 15:15:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if (someNumber <= 400){
+    return "This one is on me!";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (someNumber > 400 && someNumber < 2000){
+  return "That will be twenty bucks.";
 }
 
-function switchOnCharmFromTip(){
+else if (someNumber > 2000 && someNumber < 2500) {
+  return "I will gladly take your thirty bucks.";
+}
+
+else if(someNumber > 2500){
+  return "No can do.";
+}
+
+
+}
+
+function ternaryCheckCity(city){
+  // Write your code here!
+
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+}
+
+else{
+  return "No go.";
+}
+
+  
+}
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+  }
+  else if (tip === "not as generous") {
+    return "Thank you."; }
+  else {
+    return "Bye.";
+    }
+
   // Write your code here!
 }
